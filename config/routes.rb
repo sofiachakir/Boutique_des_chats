99ac7b204d604827ef9f8ec678c1a45b31b0d9c9
AtheDev <athe.dev@gmail.com>
2020-06-01 13:10:56 +0200

    add_show_items_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root :to => "items#show"
+  get 'items/index'
+  get 'items/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
