ebb78491a637985b73dc6de9cf5372626d14a145
EmilieDufau <dufau.emilie@gmail.com>
2020-06-01 11:44:25 +0200

    add first seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do 
+	Item.create!(
+		title: Faker::Marketing.buzzwords, 
+		description: Faker::Lorem.paragraph,
+    	price: [0, 5, 8, 10, 12, 15, 20, 50].sample,
+    	image_url: Faker::Image.mageUrl,
+		)
+end
