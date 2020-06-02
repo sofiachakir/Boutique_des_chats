7538996247a2fb20ebf10d7df75adba6efcffd40
sofiachakir <sofiachakir@gmail.com>
2020-06-02 11:36:38 +0200

    calculate total price

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,12 @@ class Cart < ApplicationRecord
 	belongs_to :user
 	has_many :cart_item_joins
 	has_many :items, through: :cart_item_joins
+
+	def total_price
+		prices = []
+		self.items.each do |item|
+			prices << item.price
+		end
+		prices.reduce(:+)
+	end
 end
