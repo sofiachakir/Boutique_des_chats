cedd6be2464db67877a808420808f2a56354c50b
sofiachakir <sofiachakir@gmail.com>
2020-06-02 17:47:02 +0200

    update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,6 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
-    @user = User.find_by(email: "anonymous@yopmail.com")
-    @cart = @user.cart
   end
 
   def show
