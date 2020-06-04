0433bc378fe0f58d3cee1dfe12157182c108748e
sofiachakir <sofiachakir@gmail.com>
2020-06-04 13:16:26 +0200

    rename routes for user profile and cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,56 +3,51 @@ class OrdersController < ApplicationController
 
   def new
     @order = Order.new
-    @cart = Cart.find(params[:cart_id])
-    @amount = Cart.find(params[:cart_id]).total_price
-    @amount = @amount * 100
+    @cart = current_user.cart
+    @amount = @cart.total_price * 100
   end
 
   def create
-    @cart_item_join = CartItemJoin.where(cart_id: params[:cart_id])
-    @cart = Cart.find(params[:cart_id])
-    @amount = Cart.find(params[:cart_id]).total_price
-    @amount = @amount * 100
+    @cart = current_user.cart
+    @amount = @cart.total_price * 100
+    @cart_item_joins = @cart.cart_item_joins
 
     if @amount != 0
+      customer = Stripe::Customer.create({
+        email: params[:stripeEmail],
+        source: params[:stripeToken],
+      })
 
-    customer = Stripe::Customer.create({
-      email: params[:stripeEmail],
-      source: params[:stripeToken],
-    })
+      charge = Stripe::Charge.create({
+        customer: customer.id,
+        amount: @amount.to_i,
+        description: 'Rails Stripe customer',
+        currency: 'eur',
+      })
 
-    charge = Stripe::Charge.create({
-      customer: customer.id,
-      amount: @amount.to_i,
-      description: 'Rails Stripe customer',
-      currency: 'eur',
-    })
-
-    @order = Order.new(user_id: current_user.id,
-                        stripe_customer_id: customer.id)
-      if @order.save
-
-        @cart.items.each do |item|
-          OrderItemJoin.create(item_id: item.id,
-                            order_id: @order.id)
-        end
-        @cart_item_join.each do |ctj|
-          ctj.destroy
-        end
+      @order = Order.new(user_id: current_user.id,
+                          stripe_customer_id: customer.id)
+        if @order.save
+          @cart.items.each do |item|
+            OrderItemJoin.create(item_id: item.id,
+                              order_id: @order.id)
+          end
+          @cart_item_joins.each do |ctj|
+            ctj.destroy
+          end
       end
+
       # Envoyer mail de confirmation à l'utilisateur et à l'admin
       @order.order_send
       @order.send_confirmation_to_admin
+
       flash[:success] = "Votre commande a été livrée par mail"
       redirect_to root_path
-
-      else
-        redirect_to root_path
-      end
+    end
 
     rescue Stripe::CardError => e
     flash[:error] = e.message
-
+    redirect_to cart_path
   end
 
 end
