79fcd095ff9dab2f29a394e3f60000252cd64ba1
AtheDev <athe.dev@gmail.com>
2020-06-04 10:37:57 +0200

    add before action

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,17 @@
 class CartsController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user, only: [:show]
 
   def show
     @cart = Cart.find(params[:id])
     @all_items = @cart.items
   end
 
+  private
+
+  def authenticate_user
+    unless current_user.id == Cart.find(params[:id]).user_id
+      flash[:danger] = "Not logged in."
+      redirect_to root_path
+    end
+  end
 end
